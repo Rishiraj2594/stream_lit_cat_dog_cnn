03dbb4453e9573e4c23661045052195d097683de
Rishiraj2594 <78264424+Rishiraj2594@users.noreply.github.com>
2023-05-03 13:01:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from keras.models import load_model
 import numpy as np
 import io
 from PIL import Image
-pip install opencv-python
-import cv2
+# pip install opencv-python
+# import cv2
 
 
 #"""Load model once at running time for all the predictions"""
@@ -23,27 +23,27 @@ if uploaded_file is not None:
     data = img.resize((128, 128), Image.ANTIALIAS)
     st.image(img, caption='Image without mask')
     
-# Function to calculate mask over image
-def weighted_img(img, initial_img, α=1., β=0.5, γ=0.):
-    return cv2.addWeighted(initial_img, α, img, β, γ)
+# # Function to calculate mask over image
+# def weighted_img(img, initial_img, α=1., β=0.5, γ=0.):
+#     return cv2.addWeighted(initial_img, α, img, β, γ)
 
-# Function to process an individual image and it's mask
-def process_image_mask(image, mask):
-    # Round to closest
-    mask = np.round(mask)
+# # Function to process an individual image and it's mask
+# def process_image_mask(image, mask):
+#     # Round to closest
+#     mask = np.round(mask)
     
-    # Convert to mask image
-    zero_image = np.zeros_like(mask)
-    mask = np.dstack((mask, zero_image, zero_image))
-    mask = np.asarray(mask, np.float32)
+#     # Convert to mask image
+#     zero_image = np.zeros_like(mask)
+#     mask = np.dstack((mask, zero_image, zero_image))
+#     mask = np.asarray(mask, np.float32)
     
-    # Convert to image image
-    image = np.asarray(image, np.float32)
+#     # Convert to image image
+#     image = np.asarray(image, np.float32)
     
-    # Get the final image
-    final_image = weighted_img(mask, image)
+#     # Get the final image
+#     final_image = weighted_img(mask, image)
 
-    return final_image    
+#     return final_image    
 
 def predict():
     global data
@@ -54,8 +54,7 @@ def predict():
 
 
     pred_mask = model_unet.predict(data)
-    result = process_image_mask(data[0], pred_mask[0])
-    st.image(result, caption='Predicted image')
+    st.image(pred_mask, caption='Predicted image')
        
     st.success('This is a  masked image')
 
