cd6a7e2aa7d48c3b9e44af02c0d742a8160e30b8
Rishiraj2594 <78264424+Rishiraj2594@users.noreply.github.com>
2023-05-03 12:28:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,19 +8,41 @@ from PIL import Image
 #"""Load model once at running time for all the predictions"""
 print('[INFO] : Model loading ................')
 global model
-model = load_model('cat_dog_classifier.h5')
+model_unet = load_model('unet.h5')
 print('[INFO] : Model loaded')
 
-st.title('What is this image? :cat: :dog:')
+st.title('Lane Segmentation')
 global data
 
-uploaded_file = st.file_uploader("Upload a file to classify")
+uploaded_file = st.file_uploader("Upload a file")
 if uploaded_file is not None:
     bytes_data = uploaded_file.read()
     img = Image.open(io.BytesIO(bytes_data))
     data = img.resize((128, 128), Image.ANTIALIAS)
-    st.image(img, caption='Image to be classified')
+    st.image(img, caption='Image without mask')
     
+# Function to calculate mask over image
+def weighted_img(img, initial_img, α=1., β=0.5, γ=0.):
+    return cv2.addWeighted(initial_img, α, img, β, γ)
+
+# Function to process an individual image and it's mask
+def process_image_mask(image, mask):
+    # Round to closest
+    mask = np.round(mask)
+    
+    # Convert to mask image
+    zero_image = np.zeros_like(mask)
+    mask = np.dstack((mask, zero_image, zero_image))
+    mask = np.asarray(mask, np.float32)
+    
+    # Convert to image image
+    image = np.asarray(image, np.float32)
+    
+    # Get the final image
+    final_image = weighted_img(mask, image)
+
+    return final_image    
+
 def predict():
     global data
     data = np.expand_dims(data, axis=0)
@@ -28,18 +50,10 @@ def predict():
     # Scaling
     data = data.astype('float') / 255 
 
-    # Prediction
-    result = model.predict(data)
 
-    pred_prob = result.item()
-
-    if pred_prob > .5:
-        label = 'Dog'
-        accuracy = round(pred_prob * 100, 2)
-    else:
-        label = 'Cat'
-        accuracy = round((1 - pred_prob) * 100, 2)
-    st.image(img, caption='Predicted image')
+    pred_mask = model_unet.predict(data)
+    result = process_image_mask(data[0], pred_mask[0])
+    st.image(result, caption='Predicted image')
        
     st.success('This is a ' + label + ' predicted with confidence ' + str(accuracy))
 
