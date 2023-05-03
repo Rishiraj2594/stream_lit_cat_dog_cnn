34acc6332255a9cf39816f205c7d637a70880e30
Rishiraj2594 <78264424+Rishiraj2594@users.noreply.github.com>
2023-05-03 23:58:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -55,6 +55,7 @@ def predict():
     if seg_type == "Unet":
         pred_mask = model_unet.predict(data)
         result = process_image_mask(data[0], pred_mask[0])
+        result = result.astype('float') / 255 
         st.image(result, caption='mask of image')
         st.image(data, caption=' image without mask')
 #     else:
